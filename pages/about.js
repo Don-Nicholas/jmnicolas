6269bc6781aea7feefcdca361af990ae5e46bd8c
= <=>
2022-02-28 03:15:23 +0800

    update

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+function about() {
   return (
-    <div className="max-w-7xl mx-auto p-5">
-      {/* Navigation */}
+    <div>
+              {/* Navigation */}
       <nav className="grid grid-cols-2 md:grid-cols-3 md:m-10">
         <div className="">
           <h1 className="font-semibold text-xl md:text-3xl">JM Nicolas</h1>
@@ -23,33 +23,7 @@ export default function Home() {
           <a href="/contact" className="mx-5 font-semibold md:text-xl">Contact</a>
         </div>
       </nav>
-      {/* landing page */}
-      <div className="max-w-4xl ">
-        <img src="/images/jaynicolas.jpg" alt="" className="mx-auto md:w-80 w-60 rounded-md block md:hidden" />
-
-        <div className="grid grid-cols-1 md:grid-cols-2 max-w-4xl md:flex md:items-stretch">
-          <div className="mb-5 self-center">
-            <h1 className="font-bold md:text-6xl text-xl my-3"> Freelancer and Full Stack Developer</h1>
-            <h3 className="text-md md:text-2xl">A 22 year old student seeking to enchance skills in software development in Norther Mindanao, Philippines.</h3>
-          </div>
-          <div>
-            <img src="/images/jaynicolas.jpg" alt="" className="hidden rounded-md md:block md:ml-60 md:w-96 w-60" />
-          </div>
-        </div>
-      </div>
-
-      {/* Recent Projects */}
-      <div className="grid grid-cols-1 md:grid-cols-2 max-w-4xl my-5 md:my-10">
-        <div className="md:my-5 my-3">
-          <h1 className="md:text-5xl md:mb-5 mb-3 text-3xl font-bold">Recent Projects</h1>
-          <h3 className="text-xl ">React.js Calculator</h3>
-          <a href="https://react-app-calculator-don.netlify.app/"><img src="/projects/project3.png" alt="" className="w-auto" /></a>
-        </div>
-        <div className="my-5">
-          <h3 className="text-xl ">Freshness and Spoilage Detection of Musa Acuminata using React.js App</h3>
-          <a href="https://sample-react-appx.herokuapp.com"><img src="/projects/project2.png" alt="" className="w-auto" /></a>
-        </div>
-      </div>
+      
 
       {/* Footer */}
       <footer className="mb-5 ml-5">
@@ -66,3 +40,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default about
